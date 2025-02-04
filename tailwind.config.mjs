8785159934c7224d73baeb44968e497965a65095
Sybatron <vipsister2014@gmail.com>
2025-02-04 12:40:15 +0200

    updated to tailwindcss

---
+++ tailwind.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: ["class"],
+    content: ["./design/**/*.{html,templ,go}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
