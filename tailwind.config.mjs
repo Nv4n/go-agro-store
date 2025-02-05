df9b2c902f73e0234c40026a209838884c7e12b9
Sybatron <vipsister2014@gmail.com>
2025-02-05 17:52:08 +0200

    moved design to frontend dir

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     darkMode: ["class"],
-    content: ["./design/**/*.{html,templ,go}"],
+    content: ["./frontend/**/*.{html,templ,go}"],
     theme: {
         extend: {},
     },
