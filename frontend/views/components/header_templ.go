9abf477614022e3fe4fde7ee4374a0e5809371c2
Sybatron <vipsister2014@gmail.com>
2025-02-06 21:51:14 +0200

    updated templ components

--- frontend/views/components/header_templ.go
+++ frontend/views/components/header_templ.go
@@ -8,8 +8,6 @@ package components
 import "github.com/a-h/templ"
 import templruntime "github.com/a-h/templ/runtime"
 
-var currentPageCss = "text-secondary-700"
-
 func navItem(currentPage string, targetPage string) templ.Component {
 	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
