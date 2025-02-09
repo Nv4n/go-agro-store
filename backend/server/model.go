08754f923d2e6f86e27112c74f19a244dd990cf7
Sybatron <vipsister2014@gmail.com>
2025-02-09 12:47:37 +0200

    Added general validator

--- backend/server/model.go
+++ backend/server/model.go
@@ -1,13 +1,39 @@
 package server
 
+import (
+	"github.com/go-playground/validator/v10"
+	"log/slog"
+	"regexp"
+)
+
 type UserRegister struct {
 	Email     string `json:"email" form:"email" validate:"required,email"`
 	Password  string `json:"password" form:"password" validate:"required,min=8,max=32"`
-	FirstName string `json:"first_name" form:"fname" validate:"required,alpha,min=2,max=50"`
-	LastName  string `json:"last_name" form:"lname" validate:"required,alpha,min=2,max=50"`
+	FirstName string `json:"first_name" form:"fname" validate:"required,name"`
+	LastName  string `json:"last_name" form:"lname" validate:"required,name"`
 }
 
 type UserLogin struct {
 	Email    string `json:"email" form:"email" validate:"required,email"`
 	Password string `json:"password" form:"password" validate:"required,min=8,max=32"`
+}
+
+var nameRegex = `^[A-ZА-Я][a-zа-я]{1,49}$`
+
+func nameValidator(fl validator.FieldLevel) bool {
+	match, err := regexp.MatchString(nameRegex, fl.Field().String())
+	if err != nil {
+		slog.Warn(err.Error())
+	}
+	return match
+}
+
+func NewValidator() (*validator.Validate, error) {
+	validate := validator.New()
+
+	err := validate.RegisterValidation("name", nameValidator)
+	if err != nil {
+		return nil, err
+	}
+	return validate, nil
 }
